1ca8837e4da2f8bae0dfc0a97448e226efcb1b12
George J Shea <52836004+GeorgeJShea@users.noreply.github.com>
2020-02-08 08:24:56 -0600

    version 2.1
    syntax and some cleaning of code and english

--- project1/Program.cs
+++ project1/Program.cs
@@ -2,7 +2,7 @@
 _____________________________________________________
 Created By: George Shea                     ßeta
 Created:7/2/2020
-Version: 2.0
+Version: 2.1
 Version Update: 7/2/2020
 Info:
 Draft picker for choaches to choice there new recruits
@@ -23,32 +23,31 @@ namespace TestClasses
 
             Printer(playerArray);
 
-            OpperationBoard(playerArray);
+            OperationBoard(playerArray);
 
         }
         
+        // Startup grabs json information and creates an array.
         static Players[,] StartUp()
         {
-            // this is told because how it prints it is 2 big for just the deafult screen size
+
+
+            // AS this is a bigger application i saw fit to tell them before boot up.
             Console.WriteLine("This Application works best in full screen mode");
             Console.WriteLine("Please do so once ready press return");
             Console.ReadLine();
-            // clears off this as it looks ugly
             Console.Clear();
 
-            // this string is used to store up objects to later be put in the multi array
+            // This stores the object information
             List<Players> playerList = new List<Players>();
 
-            // this holds the json information
+            // This holds the json information
             List<String> PlayerInfoLine = new List<String>();
 
             // counter
             int assignCounter = 0;
 
             // for every line in the json file it does the contents of the loop         // gets file from there
-            //
-            // IMPORTANT || change the realine to your own directory or this will not work in the slightest
-            //
             foreach (string line in System.IO.File.ReadLines(@"C:\Users\shegeoj\source\repos\TestClasses\TestClasses\playerDetails2.txt"))
             {
                 // adds in line to the array
@@ -60,10 +59,10 @@ namespace TestClasses
                 // adds new object to the list with all the json atributes
                 playerList.Add(new Players { Name = mover.Name, Origin = mover.Origin, Position = mover.Position, Price = mover.Price, Rank = mover.Rank });
 
-                assignCounter = assignCounter + 1;
+                assignCounter += 1;
             }
 
-            // uses list and simply plops in into the array
+            // Uses list to fill up the array
             Players[,] playerArray = new Players[,]
             {
                     { playerList[0], playerList[1], playerList[2], playerList[3], playerList[4]},
@@ -82,24 +81,27 @@ namespace TestClasses
 
         }
 
-        static int Printer(Players[,] playerArray)
+        // Printer prints the information to the screen.
+        static void Printer(Players[,] playerArray)
         {
-            playerArray = playerArray;
-            // row
+            // Row
             int i = 0;
             // colloumn
             int j = 0;
             // counter
             int arraySetCount = 0;
-            // this is used to see if the coach is spending money  "wisely"
-            // this stores the positions so it can be printed
+
+
+            // Positions
             string[] postions = { "QauterBack", "Running Back", "WideReciever", "DefensiveLineman", "DefensiveBack", "TightEnd", "Line Backer", "Offensive Tackles" };
-            // this stores space equivilent so it prints out nicely
+            // Positions but tpyed in spaces
             string[] postionsSpaces = { "          ", "            ", "            ", "                ", "             ", "        ", "           ", "                 " };
-            // prints out rankings formated to look pretty
+            
+            // Ranking of players
             Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}       {5,-20}", postionsSpaces[1], "1st", "2nd", "3rd", "4th", "5th");
             Console.WriteLine();
-            // simple loop to save some lines of code just prints out the object player array
+
+            // Prints out player information Name, Origin, Price
             while (arraySetCount < 7)
             {
                 Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}       {5,-20}", postionsSpaces[i], playerArray[i, j].Name, playerArray[i, j + 1].Name, playerArray[i, j + 2].Name, playerArray[i, j + 3].Name, playerArray[i, j + 4].Name);
@@ -109,136 +111,148 @@ namespace TestClasses
                 i = i + 1;
                 Console.WriteLine();
             }
-            // total cash
-            int fund = 96000000;
-            // prints out "rules and tells you how much money you have
+
+            // Rules For Picks and Funding to do so.
             Console.WriteLine("_______________________________________________________________________________________________________________________________________________________________");
             Console.WriteLine("2020 Draft Rules");
             Console.WriteLine("1: You may choose up to 5 players");
             Console.WriteLine("2: You may not go over budget");
-            Console.WriteLine("Current Funds: " + fund);
+            Console.WriteLine("Current Funds: 96000000" );
             Console.WriteLine("_______________________________________________________________________________________________________________________________________________________________");
 
-            return fund;
         }
 
-        static void OpperationBoard(Players[,] playerArray)
+        //OperationBaord allows for player picks and conclusion.
+        static void OperationBoard(Players[,] playerArray)
         {
+
             int fund = 96000000;
+            // Used for The economic coach award 
             int economic = 0;
-            /*
-             This si where things start to pick up pace ie get confusing
-             */
-            // sets up col/row to deafults to be chosen later
+
+            // Row and col for Picks
             int pickCol = 0;
             int pickRow = 0;
-            // empty array to store your choices 
+            // Empty arrays to store names,origins...
             string[] choices = { " ", " ", " ", " ", " " };
             string[] choicesO = { " ", " ", " ", " ", " " };
             string[] choicesP = { " ", " ", " ", " ", " " };
+            string[] choicesPO = { " ", " ", " ", " ", " " };
+            string[] choicesR = { " ", " ", " ", " ", " " };
+
             // counter
             int choiceCount = 0;
             Console.WriteLine("press x to conclude choosing");
-            // will loop unti either you go bust or you hit 5 players
+            // Will loop until either you run out of money or picks
             while (fund > 0 && choiceCount < 5)
             {
                 Console.WriteLine("Enter The Position(Row) You would like to choose from");
                 string pickRowstring = Console.ReadLine();
-                // this is the important up to 5 players 
-                // once you hit x you will leave the program
+
+                // Allows for no more picks
                 if (pickRowstring == "x")
                 {
                     break;
                 }
-                // needs to be converted into a int 
-                pickRow = Int32.Parse(pickRowstring);
-                // - so that it works normaly ie how most people count
-                pickRow = pickRow - 1;
-                // makes sure you pick with in the array
+                // Stops Users from inputing none numbers
+                try
+                {
+                    pickRow = Int32.Parse(pickRowstring);
+                }
+                catch{}
+
+                pickRow -= 1;
+
+                // Pick within array if fail loops back
                 if (pickRow < 8 && pickRow >= 0)
                 {
-                    // if failed to go in bounds breaks loops back to start
                     Console.WriteLine("Enter The Rank(Coloumn) You would like to choose from");
                     string pickColstring = Console.ReadLine();
-                    // once again you can leave now
+                    
                     if (pickColstring == "x")
                     {
                         break;
                     }
-                    // converts into ints for math
-                    pickCol = Int32.Parse(pickColstring);
-                    // once again converts it into how people normaly count
-                    pickCol = pickCol - 1;
-                    // makes sure in bounds if not loops you guess it again
+                    // Stops user inputing letters
+                    try
+                    {
+                        pickCol = Int32.Parse(pickColstring);
+                    }
+                    catch { }
+
+                    pickCol -= 1;
+
+                    // Makes sure inbounds and loops if fails
                     if (pickCol < 6 && pickRow >= 0)
                     {
-                        // prints it out what you picked to make sure you picked the right person
+                        // Prints Out Player Info
                         Console.WriteLine("You Choose " + playerArray[pickRow, pickCol].Name);
                         Console.WriteLine("           " + playerArray[pickRow, pickCol].Origin);
                         Console.WriteLine("           " + playerArray[pickRow, pickCol].Price);
-                        // asks to confirm that you want this player
+
+                        // Confirm user selected correct player
                         Console.WriteLine("Is that correct y/n");
                         string check = Console.ReadLine();
-                        // if yes do this if not once again back in the loop you go
-                        // double checks that picking this player will not make you go in the red
-                        // makes sure you do not pick the same player twice
+
+                        // Double checks funds to make sure you dont go in the Red
+                        // Checks if user confirmed pick
+                        // Confirms you have not picked the same user already.
                         if (fund - playerArray[pickRow, pickCol].Price > 0 && check == "y" && playerArray[pickRow, pickCol].Name != choices[0] && playerArray[pickRow, pickCol].Name != choices[1] && playerArray[pickRow, pickCol].Name != choices[2] && playerArray[pickRow, pickCol].Name != choices[3] && playerArray[pickRow, pickCol].Name != choices[4])
                         {
-                            // takes away your precisoue money
-                            fund = fund - playerArray[pickRow, pickCol].Price;
-                            // adds player into your choices into a list so it can print at the end
+                            fund -= playerArray[pickRow, pickCol].Price;
+
+                            // Used for pinter players at end
                             choices[choiceCount] = playerArray[pickRow, pickCol].Name;
                             choicesO[choiceCount] = playerArray[pickRow, pickCol].Origin;
                             choicesP[choiceCount] = Convert.ToString(playerArray[pickRow, pickCol].Price);
+                            choicesPO[choiceCount] = playerArray[pickRow, pickCol].Position;
+                            choicesR[choiceCount] = playerArray[pickRow, pickCol].Rank;
 
-                            // writes down current funds
+                            // Tells User Current Funds
                             Console.WriteLine(fund);
-                            // adds one to the choices max 5
+
+                            // Add to player Count
                             choiceCount = choiceCount + 1;
-                            // this is for the choachs who spend efficiently just adds if the player is in the top 3
+
+                            // Economic coach award || checks if user rank is high enough
                             if (playerArray[pickRow, pickCol].Rank == "1" || playerArray[pickRow, pickCol].Rank == "2" || playerArray[pickRow, pickCol].Rank == "3")
                             {
                                 economic = economic + 1;
                             }
 
                         }
-
-                        // just breaking logic
                         else
                         {
-                            Console.WriteLine("Sorry You have already choose that player");
+                            Console.WriteLine("Sorry you have already selected that player or your funding is not enough");
                         }
                     }
-                    // just breaking logic
                     else
                     {
-                        Console.WriteLine("Out of Bonds");
+                        Console.WriteLine("Please choose player with in the chart");
                     }
 
                 }
-                // just breaking logic
-                else
-                {
-
-                }
+                else { }
             }
-            // concludes the chocing phase
+            // Concludes the picking phase
             Console.WriteLine();
             Console.WriteLine("All players have been chosen have nice day");
             Console.WriteLine("Your Choices Were ");
-            // prints out what you choice for players
 
+            // Prints Out Users Choices
+            Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}", choicesPO[0] + " " + choicesR[0], choicesPO[1] + " " + choicesR[1], choicesPO[2] + " " + choicesR[2], choicesPO[3] + " " + choicesR[3], choicesPO[4] + " " + choicesR[4]);
             Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}", choices[0], choices[1], choices[2], choices[3], choices[4]);
             Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}", choicesO[0], choicesO[1], choicesO[2], choicesO[3], choicesO[4]);
             Console.WriteLine("      {0,-20}      {1,-20}      {2,-20}      {3,-20}       {4,-20}", choicesP[0], choicesP[1], choicesP[2], choicesP[3], choicesP[4]);
 
-            // just for niceness tells you left over funds
             Console.WriteLine("You Have $ " + fund + " remaining");
             Console.WriteLine();
-            // checks if you were spending wisely
+
+            // Gives the Economic award
             if (fund > 65000000 && economic > 2)
             {
-                Console.WriteLine("Great Work Coach You Were Econmic");
+                Console.WriteLine("Great Work Coach You Were Economic");
+                Console.WriteLine();
             }
             Console.WriteLine("We hope to see you next year");
             Console.WriteLine("_______________________________________________________________________________________________________________________________________________________________");
@@ -247,12 +261,12 @@ namespace TestClasses
 
         public class Players
         {
-            // super exiting variables
-            public string Name { get; set; }
-            public string Origin { get; set; }
-            public int Price { get; set; }
-            public string Position { get; set; }
-            public string Rank { get; set; }
+        // super exiting variables
+            public string Name { get; set; } = " ";
+            public string Origin { get; set; } = " ";
+            public int Price { get; set; } 
+            public string Position { get; set; } = " ";
+            public string Rank { get; set; } = " ";
             
 
             // used for testing 
